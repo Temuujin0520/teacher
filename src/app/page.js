117847d8ed939a3cf3eca2d9f296df08fdf6c7a3
Temuujin0520 <126646661+Temuujin0520@users.noreply.github.com>
2026-05-06 06:36:29 +0000

    temuujin

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-6 p-6">
     {DataJson.map((item)=>(
-      <div key={item.id}>{item.first_name} <img src={item.image}/></div>
+      <div className="border" key={item.id}>{item.first_name} <img src={item.image}/></div>
     ))}
     </div>
   );
