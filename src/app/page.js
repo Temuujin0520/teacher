88aab57c9080e67f904c3a4652bef1c478fe5892
Temuujin0520 <126646661+Temuujin0520@users.noreply.github.com>
2026-05-13 06:49:32 +0000

    rr

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-
 import DataJson from "../utils/data.json";
 
 export default function Home() {
-  console.log(DataJson);
   return (
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-6 p-6">
-    {DataJson.map((item)=>(
-      <div className="border" key={item.id}>{item.first_name} <img src={item.image}/></div>
-    ))}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-6 gap-6 p-6">
+      {DataJson.map((item) => (
+        <div key={item.id} className="text-center">
+          <img className="rounded-full w-60 h-60 object-cover mx-auto" src={item.image} alt={item.first_name}/>
+          <div>{item.first_name}</div>
+        </div>
+      ))}
     </div>
   );
 }
-  
